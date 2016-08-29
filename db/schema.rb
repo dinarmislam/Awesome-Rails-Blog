b268f71a1a287804f50d6e6b7369b36f1de0963e
Md. Dinar <dinar.cse@gmail.com>
2016-08-30 01:03:23 +0600

    image uploading with paperclip(post featured image)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160827225733) do
+ActiveRecord::Schema.define(version: 20160829184237) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -24,10 +24,14 @@ ActiveRecord::Schema.define(version: 20160827225733) do
   add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
 
   create_table "posts", force: :cascade do |t|
-    t.string   "title",      limit: 255
-    t.text     "body",       limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.string   "title",              limit: 255
+    t.text     "body",               limit: 65535
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.string   "image_file_name",    limit: 255
+    t.string   "image_content_type", limit: 255
+    t.integer  "image_file_size",    limit: 4
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
